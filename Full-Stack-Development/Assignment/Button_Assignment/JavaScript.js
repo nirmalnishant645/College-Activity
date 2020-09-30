1c301a06d5b5c3a57ee6807386325b073713b23a
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-09-30 16:12:52 +0530

    Radio Button Assignment

---
+++ Full-Stack-Development/Assignment/Button_Assignment/JavaScript.js
@@ -0,0 +1,17 @@
+﻿function validate() {
+    if (document.getElementById('pepsi').checked) {
+        var txt;
+        var r = confirm("You want to select Pepsi?");
+        if (r == true) {
+            txt = "You have selected Pepsi!";
+        }
+        document.getElementById("output").innerHTML = txt;
+    } else if (document.getElementById('coca').checked) {
+        var txt;
+        var r = confirm("You want to select Coca Cola?");
+        if (r == true) {
+            txt = "You have selected Coca Cola!";
+        }
+        document.getElementById("output").innerHTML = txt;
+    }
+}
