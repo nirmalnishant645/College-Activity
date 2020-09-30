889531d2f68ed73c4215bd940d9961e34cd98511
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-09-30 16:12:58 +0530

    Form Assignment

---
+++ Full-Stack-Development/Assignment/Form_Assignment/JavaScript.js
@@ -0,0 +1,20 @@
+﻿function validate() {
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var dob = document.getElementById("dob").value;
+    var college = document.getElementById("college").value;
+
+    if (fname == '') {
+        alert("Please enter First Name.");
+        return false;
+    } else if (lname == '') {
+        alert("Please enter Last Name");
+        return false;
+    } else if (dob == '') {
+        alert("Please enter Date of Birth")
+    } else if (college == '') {
+        alert("Please enter College Name")
+    } else {
+        alert("Thank You for Registring with us!");
+    }
+}
